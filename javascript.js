7e8b29f7288d0f5a52576212340f63945a3b16fd
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-05-07 09:56:50 +0200

    unstyled first version

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function calculatesum() {
+    var input1 = Number(document.getElementById('input_1').value);
+    var input2 = Number(document.getElementById('input_2').value);
+
+    var output = input1 + input2;
+    
+    document.getElementById('sumresult').innerHTML = output;
+}
+
+document.getElementById('sum').addEventListener('click', calculatesum);
+
+
+function calculatesubtract() {
+    var input3 = Number(document.getElementById('input_3').value);
+    var input4 = Number(document.getElementById('input_4').value);
+
+    var output = input3 - input4;
+    
+    document.getElementById('subtractresult').innerHTML = output;
+}
+
+document.getElementById('subtract').addEventListener('click', calculatesubtract);
