0519c02ba8475db0718af9f5c5811ca57f747b54
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-05-10 16:03:07 +0200

    added plus and resolve functions

--- javascript.js
+++ javascript.js
@@ -1,19 +1,49 @@
 
+var displayValue1; 
+//var operator; 
+
+
+function pressButtonPlus() {
+    displayValue1 = Number(document.getElementById('display').value);
+    //operator = 'plus';
+    document.getElementById('display').value = '+';
+}
+
+document.getElementById('button_plus').addEventListener('click', pressButtonPlus);
+
+
+function pressButtonResolve() {
+    var displayValue2 = Number(document.getElementById('display').value);
+    document.getElementById('display').value = displayValue1 + displayValue2;
+}
+
+document.getElementById('button_resolve').addEventListener('click', pressButtonResolve);
+
+
+
+
+
 function pressButton1() {
-    document.getElementById('display').innerHTML = 1;
+    document.getElementById('display').value = 1;
 }
 
 document.getElementById('button_1').addEventListener('click', pressButton1);
 
 function pressButton2() {
-    document.getElementById('display').innerHTML = 2;
+    document.getElementById('display').value = 2;
 }
 
 document.getElementById('button_2').addEventListener('click', pressButton2);
 
 function pressButton3() {
-    document.getElementById('display').innerHTML = 3;
+    document.getElementById('display').value = 3;
 }
 
 document.getElementById('button_3').addEventListener('click', pressButton3);
+
+
+
+
+
+
 
